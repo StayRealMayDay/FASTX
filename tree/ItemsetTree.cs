e8ead73fcb0ab34f0550d33178e9f7b55de96091
renhaoran <RHRxjtu@outlook.com>
2018-04-27 21:31:28 +0800

    add some note

--- tree/ItemsetTree.cs
+++ tree/ItemsetTree.cs
@@ -6,6 +6,15 @@
         /// Root node of the Itemset tree
         /// </summary>
         private ItemsetNode<T> Root = new ItemsetNode<T>();
-        
+
+        public ItemsetNode<T> AddChild(ItemsetNode<T> parent, Itemset<T> itemset, SparseIdList sparseIdList,
+            int position)
+        {
+            var newNode = new ItemsetNode<T>(itemset, parent, sparseIdList, position);
+            parent.GetChildren.Add(newNode);
+            return newNode;
+        }
+
+        public ItemsetNode<T> GetRoot => Root;
     }
 }
