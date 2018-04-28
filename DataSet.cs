4f387bcff46c6cddb7ca330edb346a17e3c2cc2e
renhaoran <RHRxjtu@outlook.com>
2018-04-28 15:50:50 +0800

    add read data

---
+++ DataSet.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FASTX
+{
+    public class DataSet
+    {
+        /// <summary>
+        /// the itemset separator
+        /// </summary>
+        public static string ITEMSET_SEPARATOR = "-1";
+
+        /// <summary>
+        /// the sequence separator
+        /// </summary>
+        public static string SEQUENCE_SEPARATOR = "-2";
+
+        /// <summary>
+        /// map each sequens to its SparseIdList
+        /// </summary>
+        public Dictionary<string, SparseIdList> ItemSILDic { get; private set; }
+
+        /// <summary>
+        /// Number of roes (sequences)
+        /// </summary>
+        public int NumberOfRows { get; private set; }
+
+        /// <summary>
+        /// store the min Support
+        /// </summary>
+        public int MinSupport { get; private set; }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="numberOfRows"></param>
+        /// <param name="minSupport"></param>
+        public DataSet(int numberOfRows, int minSupport)
+        {
+            NumberOfRows = numberOfRows;
+            MinSupport = minSupport;
+            ItemSILDic = new Dictionary<string, SparseIdList>();
+        }
+
+        public DataSet ReadDate(string path, int support)
+        {
+            var data = File.ReadAllLines(path);
+            foreach (var line in data)
+            {
+                if (line.Length == 0)
+                {
+                   continue;
+                }
+
+                var transactionId = 1;
+                var itemList = line.Split(' ');
+                foreach (var item in itemList)
+                {
+                    if (item == ITEMSET_SEPARATOR)
+                    {
+                        transactionId++;
+                        continue;
+                    }
+
+                    if (item == SEQUENCE_SEPARATOR)
+                    {
+                        break;;
+                    }
+                   // SparseIdList inserted = 
+                }
+            }
+        }
+
+    }
+}
---
+++ FAST.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FASTX.tree;
+
+namespace FASTX
+{
+    public class FAST<T>
+    {
+
+
+        private void ItemsetExtension()
+        {
+            var itemsetTree = new ItemsetTree<T>();
+            var root = itemsetTree.GetRoot;
+            var queue = new List<ItemsetNode<T>>();
+            var position = 0;
+            ItemsetNode<T> node;
+            
+        }
+    }
+}
