1a8e9eb7f749f11c2a309cc48df90f25193530b7
renhaoran <RHRxjtu@outlook.com>
2018-04-28 14:57:04 +0800

    add some note in the sequence tree file

--- tree/SequenceTree.cs
+++ tree/SequenceTree.cs
@@ -4,21 +4,40 @@ namespace FASTX.tree
 {
     public class SequenceTree<T>
     {
-
+        /// <summary>
+        /// root node of the sequence tree
+        /// </summary>
         public SequenceNode<T> Root { get; private set; }
 
+        /// <summary>
+        /// init a seqnence tree ,actually init the root node ,the root node do not have VIL or Parent 
+        /// </summary>
+        /// <param name="NumSequences"></param>
         public SequenceTree(int NumSequences)
         {
             Root = new SequenceNode<T>(null, new Sequence<T>(), null,  NumSequences);
         }
 
+        /// <summary>
+        /// a function to add node to the sequence tree
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="sequence"></param>
+        /// <param name="verticalIdList"></param>
+        /// <param name="support"></param>
+        /// <returns></returns>
         public SequenceNode<T> AddChild(SequenceNode<T> parent, Sequence<T> sequence, VerticalIdList verticalIdList, int support)
         {
             var newNode = new SequenceNode<T>(verticalIdList, sequence, parent, support);
             parent.GetChildren.Add(newNode);
             return newNode;
         }
-
+        
+        /// <summary>
+        /// use a List to store all the sequence node with the DFS
+        /// </summary>
+        /// <param name="Tree"></param>
+        /// <returns></returns>
         public static List<SequenceNode<T>> Visit(SequenceTree<T> Tree)
         {
             var queue = new Queue<SequenceNode<T>>();
