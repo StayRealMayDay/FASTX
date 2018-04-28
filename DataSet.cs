88d845a5500c25a4bf59d8b08b2ff96b32fc6cdf
renhaoran <RHRxjtu@outlook.com>
2018-04-28 16:44:56 +0800

    finished read data from file

--- DataSet.cs
+++ DataSet.cs
@@ -42,9 +42,11 @@ namespace FASTX
             ItemSILDic = new Dictionary<string, SparseIdList>();
         }
 
-        public DataSet ReadDate(string path, int support)
+        public static DataSet ReadDate(string path, int support)
         {
             var data = File.ReadAllLines(path);
+            var dataSet = new DataSet(data.Length, support);
+            int lineNumber = 0;
             foreach (var line in data)
             {
                 if (line.Length == 0)
@@ -66,9 +68,32 @@ namespace FASTX
                     {
                         break;;
                     }
-                   // SparseIdList inserted = 
+
+                    if (!dataSet.ItemSILDic.ContainsKey(item))
+                    {
+                        dataSet.ItemSILDic.Add(item, new SparseIdList(data.Length));
+                    }
+                    dataSet.ItemSILDic[item].AddElement(lineNumber, transactionId);
+                }
+                lineNumber++;
+            }
+
+            dataSet.ComputeFrequentItems();
+            return dataSet;
+        }
+
+        public void ComputeFrequentItems()
+        {
+            var newDic = new Dictionary<string, SparseIdList>();
+            foreach (var KeyValue in ItemSILDic)
+            {
+                if (KeyValue.Value.Support > MinSupport)
+                {
+                    newDic.Add(KeyValue.Key, KeyValue.Value);
                 }
             }
+
+            ItemSILDic = newDic;
         }
 
     }
