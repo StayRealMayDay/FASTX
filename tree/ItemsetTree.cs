e860b2286b96716b7ed9d9c131d768b8c0353f3f
renhaoran <RHRxjtu@outlook.com>
2018-04-29 16:37:28 +0800

    add some note

--- tree/ItemsetTree.cs
+++ tree/ItemsetTree.cs
@@ -9,6 +9,16 @@ namespace FASTX.tree
         /// </summary>
         private ItemsetNode<T> Root = new ItemsetNode<T>();
 
+        /// <summary>
+        /// add a node to the Itemset Tree and you neet provide the father node of this insert node
+        /// and the itemset of this node and its SIL and the position of this insert node in its father node's
+        /// children node
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="itemset"></param>
+        /// <param name="sparseIdList"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
         public ItemsetNode<T> AddChild(ItemsetNode<T> parent, Itemset<T> itemset, SparseIdList sparseIdList,
             int position)
         {
@@ -19,11 +29,19 @@ namespace FASTX.tree
 
         public ItemsetNode<T> GetRoot => Root;
 
+        /// <summary>
+        /// print the itemset tree
+        /// </summary>
         public void PrintTheTree()
         {
             Display(Root, "--");
         }
 
+        /// <summary>
+        /// recusive print every node of the tree
+        /// </summary>
+        /// <param name="itemsetNode"></param>
+        /// <param name="gap"></param>
         private void Display(ItemsetNode<T> itemsetNode, string gap)
         {
             if (itemsetNode.GetChildren.Count == 0) return;
