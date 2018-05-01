b7fcfb15a390fba884ff3772a87ea65c11a1a972
renhaoran <RHRxjtu@outlook.com>
2018-05-01 19:35:08 +0800

    add some note

--- VerticalIdList.cs
+++ VerticalIdList.cs
@@ -8,8 +8,14 @@
             Support = support;
         }
 
+        /// <summary>
+        /// store the support of this VIL
+        /// </summary>
         public int Support { get; private set; }
 
+        /// <summary>
+        /// a array to store the VIL's ListNodes
+        /// </summary>
         public ListNode[] Elements { get; private set; }
         
         
